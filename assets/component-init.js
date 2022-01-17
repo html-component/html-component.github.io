c712eee4e32ea77836319c8b6788c9d7d37717aa
r.ehdals <57896501+kangdongmandoo@users.noreply.github.com>
2022-01-17 15:20:47 +0900

    Update component-init.js

--- assets/component-init.js
+++ assets/component-init.js
@@ -1,2 +1,71 @@
-!function(){"use strict";window.Each=(e,t)=>{const n=document.getElementById(e),o=n.innerHTML,r=n.getAttribute("c:return-v"),c=n.getAttribute("c:return-i");let s="";n.innerHTML="",t.forEach((e,t)=>{const n=o.replace(new RegExp(`{{${r}}}`,"g"),e);t=n.replace(new RegExp(`{{${c}}}`,"g"),t);s+=t}),n.outerHTML=s},window.WhenLoad=e=>{"loading"===document.readyState?document.addEventListener("DOMContentLoaded",e):e()},window.Insert=(e,t)=>{const n=document.getElementById(e);let o=n.innerHTML;Object.keys(t).forEach(e=>{o=o.replace(new RegExp(`{{${e}}}`,"g"),t[e])}),n.innerHTML=o},window.ComponentFetch=(e,t)=>{const n=document.getElementById(e);e=n.getAttribute("src");e?fetch(e).then(e=>{t(n,e)}).catch(e=>console.error(e)):console.error("Fetch: No src attribute found")},window.ComponentBase=class{constructor(e,t,n){customElements.define(e,class extends HTMLElement{constructor(){super(),n?this.innerHTML=t:this.outerHTML=t}})}}}();
+!(function () {
+  'use strict';
+  (window.Each = (e, t) => {
+    const n = document.getElementById(e),
+      r = n.innerHTML,
+      o = n.getAttribute('c:return-v'),
+      c = n.getAttribute('c:return-i');
+    let s = '';
+    (n.innerHTML = ''),
+      t.forEach((e, t) => {
+        const n = r.replace(new RegExp(`{{${o}}}`, 'g'), e);
+        t = n.replace(new RegExp(`{{${c}}}`, 'g'), t);
+        s += t;
+      }),
+      (n.outerHTML = s);
+  }),
+    (window.WhenLoad = (e) => {
+      'loading' === document.readyState ? document.addEventListener('DOMContentLoaded', e) : e();
+    }),
+    (window.Insert = (e, t) => {
+      const n = document.getElementById(e);
+      let r = n.innerHTML;
+      Object.keys(t).forEach((e) => {
+        r = r.replace(new RegExp(`{{${e}}}`, 'g'), t[e]);
+      }),
+        (n.innerHTML = r);
+    }),
+    (window.ComponentFetch = (e, t) => {
+      const n = document.getElementById(e);
+      e = n.getAttribute('src');
+      e
+        ? fetch(e)
+            .then((e) => {
+              t(n, e);
+            })
+            .catch((e) => console.error(e))
+        : console.error('Fetch: No src attribute found');
+    }),
+    (window.ComponentBase = class {
+      constructor(e, t, n) {
+        customElements.define(
+          e,
+          class extends HTMLElement {
+            constructor() {
+              super(), n ? (this.innerHTML = t) : (this.outerHTML = t);
+            }
+          }
+        );
+      }
+    }),
+    (window.ComponentEach = class {
+      constructor(e) {
+        this.element = document.getElementById(e);
+      }
+      each(e) {
+        const t = this['element'],
+          r = t.innerHTML,
+          o = t.getAttribute('c:return-v'),
+          c = t.getAttribute('c:return-i');
+        let s = '';
+        (t.innerHTML = ''),
+          e.forEach((e, t) => {
+            const n = r.replace(new RegExp(`{{${o}}}`, 'g'), e);
+            t = n.replace(new RegExp(`{{${c}}}`, 'g'), t);
+            s += t;
+          }),
+          (t.outerHTML = s);
+      }
+    });
+})();
 //# sourceMappingURL=component-init.js.map
